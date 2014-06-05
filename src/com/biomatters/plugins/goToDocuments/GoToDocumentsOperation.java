db9bbc978d51c120ad833b95e6d19ebd1ac73227
Richard Moir <richard@biomatters.com>
2014-06-06 10:51:42 +1200

    Fix copyright notice

--- src/com/biomatters/plugins/goToDocuments/GoToDocumentsOperation.java
+++ src/com/biomatters/plugins/goToDocuments/GoToDocumentsOperation.java
@@ -18,13 +18,13 @@ import java.util.List;
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class GoToDocumentsOperation extends DocumentOperation {
 
